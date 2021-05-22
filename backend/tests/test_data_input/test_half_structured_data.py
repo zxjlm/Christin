360681baa42eeca41990d22ece482ef4c1e96c7f
zxjlm <zxjlm233@gmail.com>
2021-05-22 11:56:39 +0800

    :white_check_mark: update file reader

--- backend/tests/test_data_input/test_half_structured_data.py
+++ backend/tests/test_data_input/test_half_structured_data.py
@@ -7,3 +7,27 @@
 @time: 2021/1/16 23:14
 @desc:
 """
+from werkzeug.datastructures import FileStorage
+
+from config.settings import ROOT_PATH
+
+
+def test_half_structure_data(client):
+    """
+    测试 - 半结构化数据录入
+    Args:
+        client:
+
+    Returns:
+
+    """
+
+    my_file = FileStorage(
+        stream=open(ROOT_PATH + '/datas/structural_data_json.json', "rb"),
+        filename='structural_data_json.json',
+        content_type="application/json",
+    ),
+
+    response = client.post('/main/api/v2/extract_from_json', data={'files': my_file})
+
+    assert response.status_code == 200 and len(response.json['data']) == 5 and response.json['code'] == '200'
--- backend/tests/test_data_input/test_unstructured_data.py
+++ backend/tests/test_data_input/test_unstructured_data.py
@@ -7,9 +7,18 @@
 @time: 2021/1/16 23:14
 @desc:
 """
+from werkzeug.datastructures import FileStorage
 
-#
-# def test_home(client):
-#     response = client.get('/')
-#
-#     assert response.status_code == 200
+from config.settings import ROOT_PATH
+
+
+def test_normal_data(client):
+    my_file = FileStorage(
+        stream=open(ROOT_PATH + '/datas/test_data.txt', "rb"),
+        filename="test_data.txt",
+        content_type="text/plain",
+    ),
+
+    response = client.post('/main/api/v2/extract_from_text', data={'files': my_file})
+
+    assert response.status_code == 200 and len(response.json['data']) == 4
