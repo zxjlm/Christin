1817a1a1f43e35aa6ab778fb6771f7b25a01e838
zxjlm <zxjlm233@gmail.com>
2021-05-28 10:07:58 +0800

    :pushpin: add a warning tip

--- frontend/src/pages/DataManagement/HerbManage.tsx
+++ frontend/src/pages/DataManagement/HerbManage.tsx
@@ -2,7 +2,7 @@ import TableDataManagement from '@/components/TableDataManagement'
 import {PageHeaderWrapper} from "@ant-design/pro-layout";
 
 export default () => {
-  return <PageHeaderWrapper>
+  return <PageHeaderWrapper content={'目前不建议使用这个进行数据的编辑'}>
     <TableDataManagement modelName={'Herb'}/>
   </PageHeaderWrapper>
 }
